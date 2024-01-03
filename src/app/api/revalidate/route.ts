1b2c31d8dacbbf0e672b8a68a03db8b8f04aa192
sangrapura <sangrapura@senseimap.com>
2024-01-03 11:53:33 +0000

    deeper debuggng

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,14 +2,13 @@ import { revalidatePath } from 'next/cache'
 import { NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
-    const body = await request.json()
+    const { body } = await request.json()
     const path = `/${body?.record?.id || body?.old_record?.id}`
 
     if (path) {
-        console.log('Revalidating path:', path)
         revalidatePath(path)
         //revalidatePath('/', 'layout')
-        return Response.json({ revalidated: true, time: Date.now(), body: request.body, path: `${path}` })
+        return Response.json({ revalidated: true, time: Date.now(), body: body, path: path })
     }
 
     return Response.json({
